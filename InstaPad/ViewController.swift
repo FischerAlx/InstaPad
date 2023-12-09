814a03fb0b5fa5cac421e4eaa48ae981f7d7b3a5
FischerAlx <fisheralx@icloud.com>
2023-12-09 01:01:19 +0100

    Initial Commit

---
+++ InstaPad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaPad
+//
+//  Created by Александр Фишер on 09.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
